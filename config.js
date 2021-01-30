85b40556545d13a7e2523d5af751afb3b2ebd4f6
BArman1991 <51746640+BArman1991@users.noreply.github.com>
2021-01-30 15:24:32 +0200

    without config file

---
+++ config.js
@@ -0,0 +1,1 @@
+exports.Shooter = "Стрелок";
--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,3 +1,4 @@
+var config = require('../config')
 module.exports = {
     "Our first test":""+ function (browser) {
         browser.url("https://actionqa.com");
@@ -7,11 +8,12 @@ module.exports = {
 
 
     "Click test": function(Browser){
+        const name = config.Shooter
         Browser.url("https://kinokrad.co");
         Browser.click("#storyID-341069 > div > div.postertitle > h2 > a")
         Browser.pause(2 * 1000)
         Browser.assert.visible("#fulltext")
-        Browser.setValue("#story","Стрелок")
+        Browser.setValue("#story", name)
         Browser.click("#wrapp > div.search-block > form > input.imginput")
         Browser.assert.containsText("#dle-content > div:nth-child(12) > h3 > a","Стрелок (2007)")
         Browser.pause(5*1000)
