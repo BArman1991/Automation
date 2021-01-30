0139cd1c07742a05d20f1a12d8b8bbdc2bfd83e5
BArman1991 <51746640+BArman1991@users.noreply.github.com>
2021-01-30 15:12:38 +0200

    Second Commit
    2 commit

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,6 +1,23 @@
 module.exports = {
-    "Our first test": function (browser) {
+    "Our first test":""+ function (browser) {
         browser.url("https://actionqa.com");
         browser.pause(3 * 1000);
+        browser.end()
+    },
+
+
+    "Click test": function(Browser){
+        Browser.url("https://kinokrad.co");
+        Browser.click("#storyID-341069 > div > div.postertitle > h2 > a")
+        Browser.pause(2 * 1000)
+        Browser.assert.visible("#fulltext")
+        Browser.setValue("#story","Стрелок")
+        Browser.click("#wrapp > div.search-block > form > input.imginput")
+        Browser.assert.containsText("#dle-content > div:nth-child(12) > h3 > a","Стрелок (2007)")
+        Browser.pause(5*1000)
+
+
+
+        Browser.end();
     }
 }
